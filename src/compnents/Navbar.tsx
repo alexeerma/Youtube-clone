630c785515a184200ada8fbe24a9a058990c8589
alexeerma <aleksander.eerma@hotmail.com>
2023-08-30 22:44:02 +0300

    Added some packages and started working on navbar

---
+++ src/compnents/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+import { TiMicrophone } from "react-icons/ti";
+import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoAppsSharp } from "react-icons/io5";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/index.ts
@@ -0,0 +1,14 @@
+import Home from './Home';
+import Search from './Search';
+import Watch from './Watch';
+
+
+
+
+
+
+export {
+    Home, 
+    Search, 
+    Watch 
+}
