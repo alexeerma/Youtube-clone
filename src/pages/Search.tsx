1dbb6e412e8dce45c40332d4f1b8d3f8228a9ad7
alexeerma <aleksander.eerma@hotmail.com>
2023-09-19 15:25:11 +0300

    Styling for Search page need fixing

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -26,9 +26,7 @@ export default function Search() {
 
   return (
     <div className="max-h-screen overflow-hidden">
-      <div style={{ height: "7.5vh" }}>
-        <Navbar />
-      </div>
+      
       <div className="flex" style={{ height: "92.5vh" }}>
         <Sidebar />
         {videos.length ? (
